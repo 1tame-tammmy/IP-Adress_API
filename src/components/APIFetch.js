7dcfb5e323a18fa63f607a73d43922901789ada7
odeh <ahmadodeh67gt@gmail.com>
2023-05-15 03:10:16 +0200

    first commit API

---
+++ src/components/APIFetch.js
@@ -0,0 +1,72 @@
+import {useEffect,useState } from "react";
+import "../App.css";
+import Map from './Map';
+import SyncLoader from "react-spinners/ClockLoader";
+
+
+export default function API_Fetch() {
+
+    const [loading, setLoading] = useState(true);
+    const [api, setApi] = useState({});
+    // console.log("api",api)
+
+
+    useEffect(() => {
+const DataFetch = async () => {
+
+    try {
+        const response = await fetch('https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_UNtzK431AnwJga4kFtkqB7ya6XH2R');
+        if (!response.ok) {
+            throw new Error(`Request failed, status: ${response.status}`);
+        }
+        const data = await response.json();
+        setLoading(false)
+        setApi(data);
+        // console.log("data",data)
+    } catch (error) {
+		console.log(error);
+		}
+	};
+    DataFetch()
+}, []);
+
+
+    return (
+    <section className="App_body">
+        <div className="App_space">
+            <h3>IP Adress</h3>
+            <div className="body_elements">
+            {loading ? (
+				<SyncLoader
+					color="blue"
+					cssOverride={{ margin: "40vh auto" }}
+					loading
+					size={90}
+				/>
+			) : (
+                <>
+                <div className="map_container">
+                    < Map 
+                    api={api} 
+                    location={api && api.location&&(api.location) }
+                    lati ={api && api.location && api.location.lat && (api.location.lat)}
+                    lngi ={api && api.location && api.location.lng && (api.location.lng)} />
+                </div>
+                <div className="ip_adress">
+                    <p>ip adress : {api.ip}</p>
+                </div>
+                <div className="location_container">
+                    {/* {api && api.location && api.location.country && (           example for a logic to avoid crashing without waiting for the loading of the api to complete */}
+                    {/* <p>Country: {api.location.country}</p>*/}
+                    <p>Country: {api.location.country}</p>
+                    <p>region: {api.location.region}</p>
+                    <p>city: {api.location.city}</p>
+                    {api.location.postalCode && <p>postalCode:{api.location.postalCode}</p> }
+                    <p>timezone: {api.location.timezone}</p>
+                </div>
+                </>)}
+            </div>
+        </div>
+    </section>
+    )
+}
